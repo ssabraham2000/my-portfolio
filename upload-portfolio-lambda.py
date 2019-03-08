6fc69ff3a5fa09f9ba11a8fcf5d9991ffceb0e5e
Sonie Abraham <sonieabraham@gmail.com>
2019-03-07 18:44:49 -0600

    Use SNS to notify when portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,19 +4,29 @@ import StringIO
 import zipfile
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic("arn:aws:sns:us-east-1:075339839112:deployPortfolioTopic")
     
-    portfolio_bucket = s3.Bucket('portfolio.sonieabraham.info')
-    build_bucket = s3.Bucket('portfoliobuild.sonieabraham.info')
-    
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('buildPortfolio.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetype.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    try:
+        s3 = boto3.resource('s3')
         
+        portfolio_bucket = s3.Bucket('portfolio.sonieabraham.info')
+        build_bucket = s3.Bucket('portfoliobuild.sonieabraham.info')
+        
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('buildPortfolio.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetype.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+            
+        print "Job done!"
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio was deployed succesfully")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed succesfully")
+        raise
+    
     return 'Hello from Lambda'
